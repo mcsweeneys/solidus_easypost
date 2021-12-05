c1a9819455af892e46d4c917d6dff75b71254236
Nikky Southerland <nikky@uw.edu>
2021-12-05 11:12:00 -0800

    More removals

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     resource :active_shipping_settings, :only => ['show', 'update', 'edit']
-
-    resources :products, :only => [] do
-      resources :product_packages
-    end
   end
 end
